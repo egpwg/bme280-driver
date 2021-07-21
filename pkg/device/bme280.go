282984c621202b0f560e8cd47fca0bcee76308bd
Yumeng <Yumeng.pan@huaqin.com>
2021-07-21 16:39:45 +0800

    update sensor value calculation

--- pkg/device/bme280.go
+++ pkg/device/bme280.go
@@ -152,7 +152,7 @@ func (b *Bme280) GetTemperatureValue() (temperature float32, err error) {
 
 	tRaw := int32(buf[0])<<12 | int32(buf[1])<<4 | int32(buf[2])>>4
 	_, t := b.calib.compensateTemperatureInt32(tRaw)
-	temperature = float32(t / 100)
+	temperature = float32(t) / 100
 
 	return
 }
@@ -181,7 +181,7 @@ func (b *Bme280) GetPressureValue() (pressure float32, err error) {
 
 	pRaw := int32(buf[0])<<12 | int32(buf[1])<<4 | int32(buf[2])>>4
 	p := b.calib.compensatePressureInt64(tFine, pRaw)
-	pressure = float32(p / 256)
+	pressure = float32(p) / 256000
 
 	return
 }
@@ -210,7 +210,7 @@ func (b *Bme280) GetHumidityValue() (humidity float32, err error) {
 
 	hRaw := int32(buf[3])<<8 | int32(buf[4])
 	h := b.calib.compensateHumidityInt32(tFine, hRaw)
-	humidity = float32(h / 1024)
+	humidity = float32(h) / 1024
 
 	return
 }
